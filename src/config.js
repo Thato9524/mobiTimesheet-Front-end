299e0c2143f9d40469e32589d7e7c54aafe23ba9
tfabiyi <95214216+tfabiyi@users.noreply.github.com>
2022-11-03 14:52:38 +0200

    deployed url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   //url: "https://api.portal.c3-dev-house.com/v1",
-  url: "http://localhost:3001/v1",
+  url: "http://uattc3backend.us-west-2.elasticbeanstalk.com/v1",
   //url: "https://uat.api.portal.c3-dev-house.com/v1",
 
   path: {
